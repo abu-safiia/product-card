3b428d2ec2855b08dbd17fe8411398f467ecd80e
abu-safiia <mambetiyarov92@gmail.com>
2026-08-01 15:04:14 +0700

    Renamed the function.

--- homework-8.js
+++ homework-8.js
@@ -72,7 +72,7 @@ console.log(allBooks);
 
 
 // Функция для задания №10
-function isPublishedAfter2000(books) {
+function getBooksWithIsRare(books) {
     return books.map(book => {
         const isRare = book.year > 2000; // редкая
         return { ...book, isRare };
@@ -84,6 +84,6 @@ const books = [
     { title: "Человек-паук: Возвращение домой", year: 2005 },
 ];
 
-const updatedBooks = isPublishedAfter2000(books);
+const updatedBooks = getBooksWithIsRare(books);
 
 console.log(updatedBooks);
